a9f8a9e6fd41ab20ece7084234057257f53f47e1
Saurabh Kaushik <saurabh.kaushik.in@gmail.com>
2024-06-14 21:59:31 +0530

    d

--- run-app.sh
+++ run-app.sh
@@ -1,12 +1,12 @@
-# python3 -m venv env
+python3 -m venv env
 export LCA_APP_ENV='development'  
 #export LCA_APP_ENV='production'  
 export GOOGLE_APPLICATION_CREDENTIALS='./config/gcp/lca-prod-key.json'
 export PORT=8080
 export ENV AI_SERVICE_URL='http://127.0.0.1:8081'
 source env/bin/activate
-#python3 -m pip install --upgrade pip
-#pip3 install -r requirements.txt --upgrade
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt --upgrade
 #python3 -m spacy download en
 python3 app-run.py 
 # deactivate
